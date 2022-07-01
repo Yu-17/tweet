ed029d9293ea1e1a757b329d81ea3664a80f382a
ss <ss@ssnoMacBook-Pro.local>
2022-07-01 23:28:41 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,13 +29,33 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    
+
+    
+  end
+
+  def destroy
+   post = Post.find(params[:id])
+   if post.destroy
+      redirect_to posts_path, notice: "削除が完了しました"
+   end
   end
 
   
 
-    private
-    def post_params
-      params.require(:post).permit(:text, :body)
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+       redirect_to post_path(post.id)
+    else
+       render 'edit'
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text, :body)
+  end
 
 end
